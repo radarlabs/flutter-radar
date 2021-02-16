53295312693a23d81735b12d658a0e26495afe64
Nick Patrick <nicholasjohnpatrick@gmail.com>
2021-02-15 21:12:00 -0500

    pk

--- example/android/app/src/main/java/io/radar/example/MainApplication.java
+++ example/android/app/src/main/java/io/radar/example/MainApplication.java
@@ -9,7 +9,7 @@ public class MainApplication extends FlutterApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        Radar.initialize(this, "org_test_pk_5857c63d9c1565175db8b00750808a66a002acb8");
+        Radar.initialize(this, "prj_test_pk_0000000000000000000000000000000000000000");
         FlutterMain.startInitialization(this);
     }
 
