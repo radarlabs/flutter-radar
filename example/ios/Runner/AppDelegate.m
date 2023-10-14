8dd029b6c1c2e68d9ca2c228905ce8cba13ee17f
gabrielalao <gabrieldeveloper1@gmail.com>
2023-10-13 20:08:52 -0500

    remove ios native initialize

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -7,7 +7,6 @@
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  [Radar initializeWithPublishableKey:@"prj_test_pk_0000000000000000000000000000000000000000"];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
