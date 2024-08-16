5fc549a7630bd9ab7f82e09d2cf3a5242f5de142
Kenny Hu <kenny.hu@radar.com>
2024-08-16 15:01:09 -0400

    add motion detection and bump version

--- ios/Classes/RadarFlutterPlugin.m
+++ ios/Classes/RadarFlutterPlugin.m
@@ -140,7 +140,7 @@
 
     NSString *publishableKey = argsDict[@"publishableKey"];
     [[NSUserDefaults standardUserDefaults] setObject:@"Flutter" forKey:@"radar-xPlatformSDKType"];
-    [[NSUserDefaults standardUserDefaults] setObject:@"3.10.0" forKey:@"radar-xPlatformSDKVersion"];
+    [[NSUserDefaults standardUserDefaults] setObject:@"3.11.0" forKey:@"radar-xPlatformSDKVersion"];
     [Radar initializeWithPublishableKey:publishableKey];
     result(nil);
 }
