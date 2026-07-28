59c019a62f3187c870d458d468a15e6de4969350
Alan <alan.charles@radar.com>
2026-07-28 14:29:43 -0700

    cleanup/modernize android

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -69,7 +69,7 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   }
 
   Future<void> initRadar() async {
-    Radar.initialize('prj_test_pk_0000000000000000000000000000000000000000');
+    Radar.initialize('prj_test_pk_7b46891aa0a8278b5acc6bbc9f227aa5c3319483');
     Radar.setUserId('flutter');
     Radar.setDescription('Flutter');
     Radar.setMetadata({'foo': 'bar', 'bax': true, 'qux': 1});
