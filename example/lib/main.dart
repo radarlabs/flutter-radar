747febdead8d11dc3753aa8fa439b3aaf7b7ac64
Alan Charles <alan.charles@radar.com>
2026-07-31 15:06:17 -0700

    cleanup (#98)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,7 +6,7 @@ import 'api_section.dart';
 import 'console.dart';
 
 const String publishableKey =
-    'prj_test_pk_7b46891aa0a8278b5acc6bbc9f227aa5c3319483';
+    'prj_test_pk_0000000000000000000000000000000000000000g';
 
 const Map<String, double> _manhattan = {
   'latitude': 40.783826,
